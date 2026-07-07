983c6e8367a0117f28a5b4b0fb7fc0b4ffb6d6ed
SarevalasaBharathkumar <sarevalasab@gmail.com>
2026-07-07 16:00:21 +0530

    components zipped using gzip

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compress: true,
   images: { formats: ["image/avif", "image/webp"] },
   async headers() {
     return [
@@ -9,6 +10,23 @@ const nextConfig = {
           {
             key: "Cache-Control",
             value: "public, max-age=31536000, immutable"
+          },
+          {
+            key: "Expires",
+            value: "Thu, 31 Dec 2037 23:55:55 GMT"
+          }
+        ]
+      },
+      {
+        source: "/draco/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable"
+          },
+          {
+            key: "Expires",
+            value: "Thu, 31 Dec 2037 23:55:55 GMT"
           }
         ]
       },
@@ -18,6 +36,23 @@ const nextConfig = {
           {
             key: "Cache-Control",
             value: "public, max-age=31536000, immutable"
+          },
+          {
+            key: "Expires",
+            value: "Thu, 31 Dec 2037 23:55:55 GMT"
+          }
+        ]
+      },
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable"
+          },
+          {
+            key: "Expires",
+            value: "Thu, 31 Dec 2037 23:55:55 GMT"
           }
         ]
       }
